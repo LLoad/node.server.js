afbd86cd252557cb708e692919ffdacd91d5f5a2
LLoad <rudtn1406@naver.com>
2019-04-28 23:29:38 +0900

    dqd

--- Server/routes/drugs.js
+++ Server/routes/drugs.js
@@ -7,11 +7,11 @@ router.get('/drug_list_from_name', function(req, res) {
     query.selectDrugFromName(req, res);
 });
 
-router.post('/drug_list_from_image', function(req, res) {
+router.get('/drug_list_from_image', function(req, res) {
     query.selectDrugFromImage(req, res);
 });
 
-router.post('/drug_list_from_text', function(req,res) {
+router.get('/drug_list_from_text', function(req,res) {
     query.selectDrugFromText(req, res);
 });
 
